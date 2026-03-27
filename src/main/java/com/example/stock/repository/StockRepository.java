5cad766e2ad08fce9a38d9ec25fab0f2ffc28671
박권익 <kinowk@naver.com>
2026-03-27 23:55:10 +0900

    feat: Stock 재고 감소 기능 구현

---
+++ src/main/java/com/example/stock/repository/StockRepository.java
@@ -0,0 +1,26 @@
+package com.example.stock.repository;
+
+import com.example.stock.domain.Stock;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class StockRepository {
+
+    private final StockJpaRepository stockJpaRepository;
+
+    public Optional<Stock> findById(Long id) {
+        return stockJpaRepository.findById(id);
+    }
+
+    public Stock saveAndFlush(Stock stock) {
+        return stockJpaRepository.saveAndFlush(stock);
+    }
+
+    public void deleteAll() {
+        stockJpaRepository.deleteAll();
+    }
+}

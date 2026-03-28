3ff77eb9779ead9b3e93f321a599941e04f5258b
박권익 <kinowk@naver.com>
2026-03-28 15:53:53 +0900

    test: Optimistic Lock를 통한 재고 동시성 테스트

---
+++ src/test/java/com/example/stock/facade/OptimisticLockStockFacadeTest.java
@@ -0,0 +1,61 @@
+package com.example.stock.facade;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class OptimisticLockStockFacadeTest {
+
+    @Autowired
+    private OptimisticLockStockFacade stockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    private Stock savedStock;
+
+    @BeforeEach
+    void beforeEach() {
+        savedStock = stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    void afterEach() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    void 재고감소_동시성이슈() throws InterruptedException {
+        // given
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.execute(() -> {
+                try {
+                    stockFacade.decrease(savedStock.getId(), 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        // then
+        Stock stock = stockRepository.findById(savedStock.getId()).orElseThrow();
+        assertThat(stock.getQuantity()).isZero();
+    }
+}

8b68a6185c6664132106804db2a76e5e651b13d2
박권익 <kinowk@naver.com>
2026-03-28 15:52:41 +0900

    feat: EnableRetry 어노테이션 적용

--- src/main/java/com/example/stock/StockApplication.java
+++ src/main/java/com/example/stock/StockApplication.java
@@ -2,8 +2,10 @@ package com.example.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class StockApplication {
 
     public static void main(String[] args) {
